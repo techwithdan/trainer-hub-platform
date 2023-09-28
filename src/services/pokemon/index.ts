8e267575b3bd61643054bad0ba039508ce0615f2
Tonegawa94 <victor1394@gmail.com>
2023-09-28 00:07:53 -0500

    Added images to all get requests for pokemon

--- src/services/pokemon/index.ts
+++ src/services/pokemon/index.ts
@@ -4,8 +4,25 @@ import { firebase_admin } from "../../firebaseApp";
 const list_of_pokemon: Pokemon[] = pokemon_db;
 const firestore = firebase_admin.firestore();
 const pokedex_collection = firestore.collection("pokedex");
-function getRandomPokemon(): Pokemon {
-  return list_of_pokemon[Math.floor(Math.random() * list_of_pokemon.length)];
+import axios from "axios";
+
+async function getRandomPokemon(): Promise<Pokemon> {
+  const index = Math.floor(Math.random() * list_of_pokemon.length);
+  const { id } = list_of_pokemon[index];
+  console.log(Number(id).toString());
+  const {
+    data: {
+      sprites: {
+        other: {
+          "official-artwork": { front_default },
+        },
+      },
+    },
+  } = await axios.get(
+    `https://pokeapi.co/api/v2/pokemon/${Number(id).toString()}`
+  );
+  list_of_pokemon[index].img = front_default;
+  return list_of_pokemon[index];
 }
 
 export async function getPokemonByFilter({
@@ -17,30 +34,115 @@ export async function getPokemonByFilter({
   weaknesses?: string;
   limit?: number;
 }): Promise<Pokemon[]> {
-  if (type && weaknesses)
-    return list_of_pokemon.filter(
-      (pokemon) =>
+  if (type && weaknesses) {
+    const filtered_type_and_weaknesses = list_of_pokemon.filter((pokemon) => {
+      if (
         pokemon.type.toLowerCase().includes(type.toLowerCase()) &&
         pokemon.weaknesses.hasOwnProperty(weaknesses)
+      ) {
+        return pokemon;
+      }
+    });
+    return Promise.all(
+      filtered_type_and_weaknesses.map(async (pokemon) => {
+        let poke_id = Number(pokemon.id).toString();
+        const {
+          data: {
+            sprites: {
+              other: {
+                "official-artwork": { front_default },
+              },
+            },
+          },
+        } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${poke_id}`);
+        return {
+          ...pokemon,
+          img: front_default,
+        };
+      })
     );
+  }
 
-  if (type)
-    return list_of_pokemon.filter((pokemon) =>
-      pokemon.type.toLowerCase().includes(type.toLowerCase())
+  if (type) {
+    const filtered_type = list_of_pokemon.filter((pokemon) => {
+      if (pokemon.type.toLowerCase().includes(type.toLowerCase())) {
+        return pokemon;
+      }
+    });
+    return Promise.all(
+      filtered_type.map(async (pokemon) => {
+        let poke_id = Number(pokemon.id).toString();
+        console.log({ poke_id, pokemon_id: pokemon.id });
+        const {
+          data: {
+            sprites: {
+              other: {
+                "official-artwork": { front_default },
+              },
+            },
+          },
+        } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${poke_id}`);
+        return {
+          ...pokemon,
+          img: front_default,
+        };
+      })
     );
+  }
 
-  if (weaknesses)
-    return list_of_pokemon.filter((pokemon) =>
+  if (weaknesses) {
+    const filtered_weakness = list_of_pokemon.filter((pokemon) =>
       pokemon.weaknesses.hasOwnProperty(weaknesses)
     );
+    return Promise.all(
+      filtered_weakness.map(async (pokemon) => {
+        let poke_id = Number(pokemon.id).toString();
 
-  return [getRandomPokemon()];
+        const {
+          data: {
+            sprites: {
+              other: {
+                "official-artwork": { front_default },
+              },
+            },
+          },
+        } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${poke_id}`);
+        return {
+          ...pokemon,
+          img: front_default,
+        };
+      })
+    );
+  }
+
+  return [await getRandomPokemon()];
 }
 
 export async function getPokemonById(id: string): Promise<Pokemon | undefined> {
   let pokemon: Pokemon | undefined;
-  const pokemon_ref = await pokedex_collection.doc(id).get();
-  if (!pokemon_ref.exists) return pokemon;
-  pokemon = pokemon_ref.data() as Pokemon;
+  try {
+    const pokemon_ref = await pokedex_collection.doc(id).get();
+    if (!pokemon_ref.exists) {
+      return undefined;
+    }
+
+    const {
+      data: {
+        sprites: {
+          other: {
+            "official-artwork": { front_default },
+          },
+        },
+      },
+    } = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${Number(id).toString()}`
+    );
+
+    pokemon = pokemon_ref.data() as Pokemon;
+    pokemon.img = front_default;
+  } catch (e) {
+    console.error(e);
+    //if error, return undefined
+  }
   return pokemon;
 }
